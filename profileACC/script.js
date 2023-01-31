9d31ace0694e25a829ff952188df62e56997794e
flazer <73180739+flazer-f@users.noreply.github.com>
2023-01-31 15:56:01 +0530

    Add files via upload
    this is an account/profile page

---
+++ profileACC/script.js
@@ -0,0 +1,17 @@
+<script>
+  // Load more photos when the "Load More" button is clicked
+  const loadMoreButton = document.getElementById('load-more-button');
+  loadMoreButton.addEventListener('click', () => {
+    // TODO: Add code to load more photos here
+  });
+
+  // Like a photo when the "Like" button is clicked
+  const likeButtons = document.querySelectorAll('.like-button');
+
+  likeButtons.forEach((button) => {
+    
+    button.addEventListener('click', () => {
+      // TODO: Add code to like the photo here
+    });
+  });
+</script>
